f4f2046d213ee244cfa55659c1d951103ab71f87
starry <3299457867@qq.com>
2026-06-11 09:32:11 +0800

    chore: register RiskModule and AiModule in AppModule with full test suite

--- apps/api/src/modules/risk/risk.module.ts
+++ apps/api/src/modules/risk/risk.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { RiskController } from './risk.controller';
 import { RiskService } from './risk.service';
 import { RiskScoringService } from './risk-scoring.service';
+import { DispatchRecommendationService } from './dispatch-recommendation.service';
 import { RulesController } from './rules/rules.controller';
 import { RulesService } from './rules/rules.service';
 
 @Module({
   controllers: [RiskController, RulesController],
-  providers: [RiskScoringService, RiskService, RulesService],
-  exports: [RiskScoringService, RiskService],
+  providers: [RiskScoringService, RiskService, RulesService, DispatchRecommendationService],
+  exports: [RiskScoringService, RiskService, DispatchRecommendationService],
 })
 export class RiskModule {}
