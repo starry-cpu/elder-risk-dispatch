25016923960113c5190cf2ba7eb7dbf3595bc5a9
starry <3299457867@qq.com>
2026-06-11 19:02:28 +0800

    feat: import AiModule and RiskModule into CheckInModule

--- apps/api/src/modules/check-ins/check-ins.module.ts
+++ apps/api/src/modules/check-ins/check-ins.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CheckInsController } from './check-ins.controller';
 import { CheckInsService } from './check-ins.service';
+import { AiModule } from '../ai/ai.module';
+import { RiskModule } from '../risk/risk.module';
 
 @Module({
+  imports: [AiModule, RiskModule],
   controllers: [CheckInsController],
   providers: [CheckInsService],
   exports: [CheckInsService],
